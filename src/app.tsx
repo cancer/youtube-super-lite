631f93aaedf1917ae86ed67c824d69f7b9c0c27e
cancer <zodiac.cancer.j6@gmail.com>
2024-04-11 19:07:51 +0900

    remove YouTubeApiClient

--- src/app.tsx
+++ src/app.tsx
@@ -9,52 +9,44 @@ import { FileRoutes } from "@solidjs/start/router";
 import { ErrorBoundary, Match, Suspense, Switch } from "solid-js";
 import { getRequestEvent, isServer } from "solid-js/web";
 import { isTokenExpired } from "~/libs/api/youtube";
-import { YouTubeApiProvider } from "~/libs/api/youtube/context";
-import { getAuthTokens } from "~/libs/session";
 import { BareLayout } from "./layouts/bare";
 
 export default function App() {
   return (
-    <YouTubeApiProvider
-      getAuthTokens={() =>
-        getAuthTokens({ secret: process.env.SESSION_SECRET! })
-      }
-    >
-      <BareLayout>
-        <ErrorBoundary
-          fallback={(err) => {
-            console.error(err);
-            return (
-              <Switch>
-                <Match when={isServer && isTokenExpired(err)}>
-                  {(_) => {
-                    const event = getRequestEvent();
-                    const redirectTo = event
-                      ? new URL(event.request.url).pathname
-                      : "/";
-                    return (
-                      <>
-                        <HttpStatusCode code={302} />
-                        <HttpHeader
-                          name="Location"
-                          value={`/login?redirect_to=${redirectTo}`}
-                        />
-                      </>
-                    );
-                  }}
-                </Match>
-                <Match when={err}>
-                  <div>Error: {err.message}</div>
-                </Match>
-              </Switch>
-            );
-          }}
-        >
-          <Router root={(props) => <Suspense> {props.children} </Suspense>}>
-            <FileRoutes />
-          </Router>
-        </ErrorBoundary>
-      </BareLayout>
-    </YouTubeApiProvider>
+    <BareLayout>
+      <ErrorBoundary
+        fallback={(err) => {
+          console.error(err);
+          return (
+            <Switch>
+              <Match when={isServer && isTokenExpired(err)}>
+                {(_) => {
+                  const event = getRequestEvent();
+                  const redirectTo = event
+                    ? new URL(event.request.url).pathname
+                    : "/";
+                  return (
+                    <>
+                      <HttpStatusCode code={302} />
+                      <HttpHeader
+                        name="Location"
+                        value={`/login?redirect_to=${redirectTo}`}
+                      />
+                    </>
+                  );
+                }}
+              </Match>
+              <Match when={err}>
+                <div>Error: {err.message}</div>
+              </Match>
+            </Switch>
+          );
+        }}
+      >
+        <Router root={(props) => <Suspense> {props.children} </Suspense>}>
+          <FileRoutes />
+        </Router>
+      </ErrorBoundary>
+    </BareLayout>
   );
 }
--- src/libs/api/youtube/index.ts
+++ src/libs/api/youtube/index.ts
@@ -1,2 +1,109 @@
-export * from "./client";
-export * from "./context";
+import type {
+  Subscription,
+  VideoGetRatingResponse,
+} from "~/libs/api/youtube/types";
+import { type AuthTokens, getAuthTokens } from "~/libs/session";
+
+export class TokenExpiredError extends Error {
+  name = "TokenExpiredError";
+
+  constructor() {
+    super("Token has expired.");
+  }
+}
+
+export const isTokenExpired = (err: unknown): err is TokenExpiredError => {
+  if (!err) return false;
+  if (!(err instanceof TokenExpiredError)) return false;
+  if (err.name !== "TokenExpiredError") return false;
+  return true;
+};
+type PageInfo = { pageInfo: { totalResults: number; resultsPerPage: number } };
+export type MyChannelsRequest = {
+  GET: { part: string[]; maxResults: number };
+};
+export type MyChannelsResponse = {
+  GET: PageInfo & {
+    items: Subscription[];
+  };
+};
+export const listMyChannels = ({
+  part,
+  maxResults,
+}: MyChannelsRequest["GET"]): Promise<MyChannelsResponse["GET"]> => {
+  "use server";
+  const params = {
+    maxResults,
+    part: part.join(","),
+    mine: true,
+  };
+  return request(() => getAuthTokens({ secret: process.env.SESSION_SECRET! }))({
+    uri: "/subscriptions",
+    method: "GET",
+    params,
+  });
+};
+export type VideoRatingRequest = {
+  GET: { id: string };
+};
+export type VideoRatingResponse = {
+  GET: { rating: string };
+};
+export const getVideoRating = async ({
+  id,
+}: VideoRatingRequest["GET"]): Promise<VideoRatingResponse["GET"]> => {
+  "use server";
+  const params = {
+    id,
+  };
+  return request(() =>
+    getAuthTokens({ secret: process.env.SESSION_SECRET! }),
+  )<VideoGetRatingResponse>({
+    uri: "/videos/getRating",
+    method: "GET",
+    params,
+  }).then((res) => {
+    if (res.items.length === 0) return { rating: "" };
+    return { rating: res.items[0].rating };
+  });
+};
+export const request = (
+  getAuthTokens: () => Promise<AuthTokens | null>,
+): (<T = unknown>(args: {
+  uri: string;
+  method: "GET" | "POST";
+  params?: Record<string, unknown>;
+  body?: Record<string, unknown>;
+}) => Promise<T>) => {
+  return async ({ uri, method, params, body }) => {
+    let tokens;
+    try {
+      tokens = await getAuthTokens();
+    } catch (err) {
+      console.error("Failed to load tokens from session.", err);
+      throw new TokenExpiredError();
+    }
+    if (tokens === null || Date.now() > tokens.expiresAt) {
+      console.error("Retrieved tokens have expired.");
+      throw new TokenExpiredError();
+    }
+
+    const url = new URL(`https://youtube.googleapis.com/youtube/v3${uri}`);
+    Object.entries(params ?? {}).forEach(([key, value]) =>
+      url.searchParams.set(key, String(value)),
+    );
+    const res = await fetch(url, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${tokens.accessToken}`,
+        Accept: "application/json",
+      },
+      body: body ? JSON.stringify(body) : undefined,
+    });
+
+    if (!res.ok) throw new Error(await res.text());
+
+    return res.json();
+  };
+};
