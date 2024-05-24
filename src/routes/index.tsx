d6bc3114ba3238fdd301168665f42d3b81c8e98a
cancer <zodiac.cancer.j6@gmail.com>
2024-05-24 11:53:00 +0900

    remove video list for reduce cloudflare sub-requests

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import {
-  A,
   action,
   cache,
   createAsync,
@@ -8,50 +7,16 @@ import {
   useAction,
   useNavigate,
 } from "@solidjs/router";
-import { createMemo, For, Match, Show, Switch } from "solid-js";
+import { createMemo, For, Show } from "solid-js";
 import { createAuthClient, revokeToken } from "~/libs/api/auth";
-import {
-  LatestVideoListRequestGet,
-  LatestVideoListResponseGet,
-  listLatestVideos,
-  listMyChannels,
-  type MyChannelsRequest,
-} from "~/libs/api/youtube";
+import { listMyChannels, type MyChannelsRequest } from "~/libs/api/youtube";
 import { createAuthTokensClient } from "~/libs/auth-tokens/client";
-import {
-  formatDurationTime,
-  getHourDiff,
-  startOfDay,
-  subtractDays,
-} from "~/libs/datetime";
 import { getSession } from "~/libs/session";
 
 const fetchChannels = cache(async (params: MyChannelsRequest["GET"]) => {
   "use server";
   return listMyChannels(params);
 }, "channels");
-
-const fetchLatestLiveStreaming = cache(
-  async (params: LatestVideoListRequestGet) => {
-    "use server";
-    const videos = await listLatestVideos(params);
-    const [live, others] = videos.items.reduce(
-      (acc: LatestVideoListResponseGet["items"][number][][], item) => {
-        if (item.snippet.liveBroadcastContent === "live") {
-          acc[0].push(item);
-          return acc;
-        }
-        // ただの動画
-        if (item.liveStreamingDetails === undefined) return acc;
-        acc[1].push(item);
-        return acc;
-      },
-      [[], []],
-    );
-    return [...live, ...others];
-  },
-  "latestVideos",
-);
 
 const logoutAction = action(async () => {
   "use server";
@@ -73,16 +38,10 @@ const logoutAction = action(async () => {
 export const route = {
   load: () => {
     return (
-      Promise.all([
-        fetchChannels({
-          part: ["snippet"],
-          maxResults: 50,
-        }),
-        fetchLatestLiveStreaming({
-          maxResults: 50,
-          publishedAfter: startOfDay(subtractDays(new Date(), 1)),
-        }),
-      ])
+      fetchChannels({
+        part: ["snippet"],
+        maxResults: 50,
+      })
         // https://github.com/solidjs/solid-router/issues/399
         .catch((err) => {
           console.error(err);
@@ -92,29 +51,13 @@ export const route = {
   },
 } satisfies RouteDefinition;
 
+//
+// !!! TODO: サブリクエストが50超えてしまっているのでなんとかしないといけない !!!
+//
 const Index = () => {
   const navigate = useNavigate();
   const channels = createAsync(
     () => fetchChannels({ part: ["snippet"], maxResults: 50 }),
-    { deferStream: true },
-  );
-  const thumbnailsMap = createMemo(() => {
-    if (!channels()) return {};
-    return channels()!.items.reduce(
-      (acc, channel) => {
-        acc[channel.snippet.resourceId.channelId] =
-          channel.snippet.thumbnails.default.url;
-        return acc;
-      },
-      {} as Record<string, string>,
-    );
-  });
-  const latestVideos = createAsync(
-    () =>
-      fetchLatestLiveStreaming({
-        maxResults: 50,
-        publishedAfter: startOfDay(subtractDays(new Date(), 1)),
-      }),
     { deferStream: true },
   );
 
@@ -151,85 +94,6 @@ const Index = () => {
                       class="w-full rounded-full"
                     />
                   </a>
-                </li>
-              )}
-            </For>
-          </ul>
-        )}
-      </Show>
-      <Show when={latestVideos()}>
-        {(data) => (
-          <ul class="w-full flex flex-wrap gap-4 overflow-x-hidden list-none p-0">
-            <For each={data()}>
-              {({ id, snippet, liveStreamingDetails, contentDetails }) => (
-                <li class="w-min object-contain grid gap-2">
-                  <div class="grid grid-cols-[auto_min-content_0.2rem] grid-rows-[auto_min-content_0.2rem]">
-                    <A
-                      href={`/watch/${id}`}
-                      class="hover-opacity-80 col-span-full row-span-full"
-                    >
-                      <img
-                        src={snippet.thumbnails.medium.url}
-                        alt={snippet.title}
-                        width={snippet.thumbnails.medium.width}
-                        classList={{
-                          "border-red": snippet.liveBroadcastContent === "live",
-                          "border-4": snippet.liveBroadcastContent === "live",
-                          "border-solid":
-                            snippet.liveBroadcastContent === "live",
-                        }}
-                      />
-                    </A>
-                    <span class="min-h-4 col-start-2 row-start-2 z-1">
-                      <Show when={snippet.liveBroadcastContent === "none"}>
-                        <span class="bg-black rounded p-1 text-xs">
-                          {formatDurationTime(contentDetails.duration)}
-                        </span>
-                      </Show>
-                    </span>
-                  </div>
-                  <A
-                    href={`/watch/${id}`}
-                    class="flex gap-xs items-center color-white decoration-none hover-decoration-underline"
-                  >
-                    <img
-                      src={thumbnailsMap()[snippet.channelId]}
-                      alt={snippet.channelTitle}
-                      class="w-8 h-8 rounded-full"
-                    />
-                    <span class="m0 line-clamp-2">{snippet.title}</span>
-                  </A>
-                  <p class="flex justify-between m0 text-xs">
-                    <span class="text-stone-400 line-clamp-1">
-                      {snippet.channelTitle}
-                    </span>
-                    <Switch>
-                      <Match when={snippet.liveBroadcastContent === "live"}>
-                        <span class="bg-red rounded w-min pl-2 pr-2">Live</span>
-                      </Match>
-                      <Match when={snippet.liveBroadcastContent === "upcoming"}>
-                        <span class="bg-stone-500 rounded w-min pl-2 pr-2 text-nowrap">
-                          {new Intl.DateTimeFormat("ja-JP", {
-                            month: "numeric",
-                            day: "numeric",
-                            hour: "numeric",
-                            minute: "numeric",
-                          }).format(
-                            new Date(liveStreamingDetails.scheduledStartTime),
-                          )} 〜
-                        </span>
-                      </Match>
-                      <Match when={snippet.liveBroadcastContent === "none"}>
-                        {new Intl.RelativeTimeFormat("ja-JP").format(
-                          getHourDiff(
-                            new Date(liveStreamingDetails.actualEndTime),
-                            new Date(),
-                          ),
-                          "hour",
-                        )}に配信済み
-                      </Match>
-                    </Switch>
-                  </p>
                 </li>
               )}
             </For>
