0ac53cff64650edfc7d41d2783d22f4fcc9625f3
cancer <zodiac.cancer.j6@gmail.com>
2024-11-07 10:23:22 +0900

    add support for parsing youtu.be

--- src/libs/url.ts
+++ src/libs/url.ts
@@ -2,16 +2,15 @@ type VideoNavigation = { type: "video"; id: string };
 type UnknownNavigation = { type: "unknown" };
 export type YTNavigation = VideoNavigation | UnknownNavigation;
 
-// TODO: https://youtu.be/2wczkeeoYQc にも対応できるように
-type ParseYouTubeUrl = (
-  urlStr: string,
-) => YTNavigation;
+type ParseYouTubeUrl = (urlStr: string) => YTNavigation;
 export const parseYouTubeUrl: ParseYouTubeUrl = (urlStr) => {
   const url = new URL(urlStr);
 
-  if (url.pathname === "/watch") {
+  if (url.hostname === "youtu.be")
+    return { type: "video", id: url.pathname.replace("/", "") };
+
+  if (url.pathname === "/watch")
     return { type: "video", id: url.searchParams.get("v")! };
-  }
 
   return { type: "unknown" };
 };
